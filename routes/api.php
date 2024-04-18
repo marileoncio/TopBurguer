7c23cd6cb017c949204cd3f601ecff96a1e93bb0
marileoncio <mariana.timoteo19@yahoo.com>
2024-04-18 13:17:01 -0300

    commit

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProdutoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/produtos',[ProdutoController::class, 'index']);
+Route::get('/produto',[ProdutoController::class, 'index']);
 Route::post('/produtos',[ProdutoController::class, 'store']);
 
 Route::get('/cliente',[ClienteController::class, 'index']);
